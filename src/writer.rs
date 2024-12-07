0db0fb0b0a2710c613eee693271fec447f3f9a00
Konrad Goławski <konrad@golawski.it>
2024-12-07 18:32:22 +0100

    New version

--- src/writer.rs
+++ src/writer.rs
@@ -8,7 +8,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! chksum-md5 = { version = "0.0.0", features = ["writer"] }
+//! chksum-md5 = { version = "0.1.0", features = ["writer"] }
 //! ```
 //!
 //! Alternatively, use the [`cargo add`](https://doc.rust-lang.org/cargo/commands/cargo-add.html) subcommand:
